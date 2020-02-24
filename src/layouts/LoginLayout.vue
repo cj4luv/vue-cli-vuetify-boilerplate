ab6f423dc398c413ae0c5074c1e6f1a913a9954d
laon <cj4luv@gmail.com>
2020-02-24 14:10:50 +0900

    init

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -22,41 +22,12 @@
               >
                 <v-toolbar-title>Login form</v-toolbar-title>
                 <v-spacer />
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      :href="source"
-                      icon
-                      large
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      icon
-                      large
-                      href="https://codepen.io/johnjleider/pen/pMvGQO"
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-codepen</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Codepen</span>
-                </v-tooltip>
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-text-field
                     label="Login"
                     name="login"
-                    prepend-icon="person"
                     type="text"
                   />
 
@@ -64,14 +35,13 @@
                     id="password"
                     label="Password"
                     name="password"
-                    prepend-icon="lock"
                     type="password"
                   />
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary">Login</v-btn>
+                <v-btn color="primary" @click="onLogin">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -83,8 +53,16 @@
 
 <script>
   export default {
-    props: {
-      source: String,
+    data() {
+      return {
+        id: '',
+        pw: '',
+      };
+    },
+    methods: {
+      onLogin() {
+        this.$router.push('/home');
+      },
     },
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueCookies from 'vue-cookies';
 
 import vuetify from '@plugins/vuetify';
 import router from '@plugins/router';
@@ -8,6 +9,7 @@ import router from '@plugins/router';
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
+Vue.use(VueCookies);
 
 new Vue({
   vuetify,
